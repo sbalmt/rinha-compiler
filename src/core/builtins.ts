e0a9d16f43577eb6fb9e90bf766d571a4cb275a5
Silas Balmante <silas@balmante.eti.br>
2023-09-21 18:26:26 -0300

    refactor: break down builtins

--- src/core/builtins.ts
+++ src/core/builtins.ts
@@ -1,5 +1,6 @@
-import * as Expression from '../nodes/expression';
+import * as Core from '@xcheme/core';
 
+import * as Expression from '../nodes/expression';
 import * as Errors from './errors';
 
 import { Metadata } from './metadata';
@@ -7,26 +8,30 @@ import { convertToString } from './converters';
 import { ErrorTypes } from './types';
 import { Scope } from './scope';
 
+const printFn = (scope: Scope<Metadata>, node: Core.Node<Metadata>) => {
+  const result = Expression.consumeNode(scope, node);
+  console.log(convertToString(result));
+  return result;
+};
+
+const firstFn = (scope: Scope<Metadata>, node: Core.Node<Metadata>) => {
+  const tuple = Expression.consumeNode(scope, node);
+  if (!(tuple instanceof Array)) {
+    throw Errors.getMessage(ErrorTypes.INVALID_TUPLE, node.fragment);
+  }
+  return tuple[0];
+};
+
+const secondFn = (scope: Scope<Metadata>, node: Core.Node<Metadata>) => {
+  const tuple = Expression.consumeNode(scope, node);
+  if (!(tuple instanceof Array)) {
+    throw Errors.getMessage(ErrorTypes.INVALID_TUPLE, node.fragment);
+  }
+  return tuple[1];
+};
+
 export const injectBuiltIns = (scope: Scope<Metadata>): void => {
-  scope.createCustomVariable('print', (scope, node) => {
-    const result = Expression.consumeNode(scope, node);
-    console.log(convertToString(result));
-    return result;
-  });
-
-  scope.createCustomVariable('first', (scope, node) => {
-    const tuple = Expression.consumeNode(scope, node);
-    if (!(tuple instanceof Array)) {
-      throw Errors.getMessage(ErrorTypes.INVALID_TUPLE, node.fragment);
-    }
-    return tuple[0];
-  });
-
-  scope.createCustomVariable('second', (scope, node) => {
-    const tuple = Expression.consumeNode(scope, node);
-    if (!(tuple instanceof Array)) {
-      throw Errors.getMessage(ErrorTypes.INVALID_TUPLE, node.fragment);
-    }
-    return tuple[1];
-  });
+  scope.createCustomVariable('print', printFn);
+  scope.createCustomVariable('first', firstFn);
+  scope.createCustomVariable('second', secondFn);
 };
