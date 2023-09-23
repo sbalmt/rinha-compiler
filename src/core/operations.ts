ba5014388e6902fd094756015c463239c747ae61
Silas Balmante <silas@balmante.eti.br>
2023-09-23 14:06:51 -0300

    fix: ensure int32 for resulting operations

--- src/core/operations.ts
+++ src/core/operations.ts
@@ -4,18 +4,18 @@ import { ensureInt32 } from './converters';
 export const resolveArithmeticOperation = (lhs: number, rhs: number, operation: ArithmeticNodeTypes) => {
   switch (operation) {
     case NodeTypes.ADD:
-      return ensureInt32(lhs) + ensureInt32(rhs);
+      return ensureInt32(lhs + rhs);
 
     case NodeTypes.SUBTRACT:
-      return ensureInt32(lhs) - ensureInt32(rhs);
+      return ensureInt32(lhs - rhs);
 
     case NodeTypes.MULTIPLY:
-      return ensureInt32(lhs) * ensureInt32(rhs);
+      return ensureInt32(lhs * rhs);
 
     case NodeTypes.DIVIDE:
-      return Math.trunc(ensureInt32(lhs) / ensureInt32(rhs));
+      return ensureInt32(Math.trunc(lhs / rhs));
 
     case NodeTypes.MODULO:
-      return ensureInt32(lhs) % ensureInt32(rhs);
+      return ensureInt32(lhs % rhs);
   }
 };
