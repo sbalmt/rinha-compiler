4a914f26e2f1a2c9bb2aafc6d08d1bbc7145a26b
Silas Balmante <silas@balmante.eti.br>
2023-09-25 02:18:22 -0300

    chore: improve block nodes

--- src/evaluator/nodes/block.ts
+++ src/evaluator/nodes/block.ts
@@ -8,37 +8,46 @@ import { Metadata } from '../../core/metadata';
 import { NodeTypes } from '../../core/types';
 import { Scope, VarValueType } from '../scope';
 
+const consumeInnerNode = (scope: Scope<Metadata>, node: Core.Node<Metadata>) => {
+  const innerScope = new Scope(scope, scope.options);
+  return consumeNodes(innerScope, node);
+};
+
+const consumeSingleNode = (scope: Scope<Metadata>, node: Core.Node<Metadata>): VarValueType<Metadata> => {
+  switch (node.value) {
+    case NodeTypes.EXPRESSION:
+      return Expression.consumeNode(scope, node.right!);
+
+    case NodeTypes.VARIABLE:
+      Variable.consumeNode(scope, node.right!);
+      break;
+
+    case NodeTypes.IF_ELSE:
+      return Condition.consumeNode(scope, node.right!);
+
+    case NodeTypes.BLOCK:
+      return consumeInnerNode(scope, node.right!);
+
+    default:
+      throw `Unexpected block node type (${node.value}).`;
+  }
+
+  return undefined;
+};
+
 export const consumeNodes = (scope: Scope<Metadata>, node: Core.Node<Metadata>): VarValueType<Metadata> => {
   const { debug } = scope.options;
 
-  let result;
+  let value;
 
-  do {
+  while (node) {
     if (debug) {
       console.log('EVL', node.value, node.fragment.data);
     }
 
-    switch (node.value) {
-      case NodeTypes.EXPRESSION:
-        result = Expression.consumeNode(scope, node.right!);
-        break;
+    value = consumeSingleNode(scope, node);
+    node = node.next!;
+  }
 
-      case NodeTypes.VARIABLE:
-        Variable.consumeNode(scope, node.right!);
-        break;
-
-      case NodeTypes.IF_ELSE:
-        result = Condition.consumeNode(scope, node.right!);
-        break;
-
-      case NodeTypes.BLOCK:
-        result = consumeNodes(scope, node.right!);
-        break;
-
-      default:
-        throw `Unexpected block node type (${node.value}).`;
-    }
-  } while ((node = node.next!));
-
-  return result;
+  return value;
 };
--- src/optimizer/nodes/block.ts
+++ src/optimizer/nodes/block.ts
@@ -6,46 +6,40 @@ import * as Condition from './condition';
 
 import { Metadata } from '../../core/metadata';
 import { NodeTypes } from '../../core/types';
-import { Scope, ScopeTypes } from '../scope';
+import { Scope } from '../scope';
 
-const consumeInnerNodes = (scope: Scope, node: Core.Node<Metadata>) => {
+const consumeSingleNode = (scope: Scope, node: Core.Node<Metadata>) => {
+  switch (node.value) {
+    case NodeTypes.EXPRESSION:
+      Expression.consumeNode(scope, node.right!);
+      break;
+
+    case NodeTypes.VARIABLE:
+      Variable.consumeNode(node, node.right!);
+      break;
+
+    case NodeTypes.IF_ELSE:
+      Condition.consumeNode(scope, node, node.right!);
+      break;
+
+    case NodeTypes.BLOCK:
+      consumeNodes(scope, node.right!);
+      break;
+
+    default:
+      throw `Unexpected block node type (${node.value}).`;
+  }
+};
+
+export const consumeNodes = (scope: Scope, node: Core.Node<Metadata>) => {
   const { debug } = scope.options;
 
-  do {
+  while (node) {
     if (debug) {
       console.log('OPT', node.value, node.fragment.data);
     }
 
-    switch (node.value) {
-      case NodeTypes.EXPRESSION:
-        Expression.consumeNode(scope, node.right!);
-        break;
-
-      case NodeTypes.VARIABLE:
-        Variable.consumeNode(node, node.right!);
-        break;
-
-      case NodeTypes.IF_ELSE:
-        Condition.consumeNode(scope, node, node.right!);
-        break;
-
-      case NodeTypes.BLOCK:
-        consumeInnerNodes(scope, node.right!);
-        break;
-
-      default:
-        throw `Unexpected block node type (${node.value}).`;
-    }
+    consumeSingleNode(scope, node);
     node = node.next!;
-  } while (node);
-};
-
-export const consumeNodes = (scope: Scope, node: Core.Node<Metadata>) => {
-  const type = scope.type;
-  scope.type = ScopeTypes.BLOCK;
-
-  const result = consumeInnerNodes(scope, node);
-  scope.type = type;
-
-  return result;
+  }
 };
