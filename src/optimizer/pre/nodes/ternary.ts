1dc58287e5bf098cf6923379b5647358a86df60c
Silas Balmante <silas@balmante.eti.br>
2023-09-25 17:54:18 -0300

    fix: disable optimizations for ternary if-else

--- src/optimizer/pre/nodes/ternary.ts
+++ src/optimizer/pre/nodes/ternary.ts
@@ -1,12 +1,32 @@
 import * as Core from '@xcheme/core';
 
-import * as Condition from './condition';
+import * as Expression from './expression';
+import * as Block from './block';
 
 import { Metadata } from '../../../core/metadata';
 import { Scope } from '../../scope';
 
+const consumeInnerNode = (scope: Scope, node: Core.Node<Metadata>) => {
+  const blockScope = new Scope(node, Core.NodeDirection.Right, scope.options);
+  blockScope.declarationNode = scope.declarationNode;
+  Block.consumeNodes(blockScope, blockScope.currentNode);
+};
+
 export const consumeNode = (scope: Scope, node: Core.Node<Metadata>) => {
-  Condition.consumeNode(scope, node.right!);
+  const conditionNode = node.right!;
+
+  Expression.consumeNode(scope, conditionNode.right!);
+
+  const successBlock = conditionNode.next!;
+  const failureBlock = successBlock.next!;
+
+  if (successBlock.right) {
+    consumeInnerNode(scope, successBlock);
+  }
+
+  if (failureBlock && failureBlock.right) {
+    consumeInnerNode(scope, failureBlock);
+  }
 
   return undefined;
 };
