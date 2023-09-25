21e93629944000066eeebbd503aaa2af3180ac05
Silas Balmante <silas@balmante.eti.br>
2023-09-25 03:04:16 -0300

    chore: remove unnecessary code

--- src/optimizer/nodes/variable.ts
+++ src/optimizer/nodes/variable.ts
@@ -7,16 +7,7 @@ import { Scope } from '../scope';
 
 export const consumeNode = (parent: Core.Node<Metadata>, node: Core.Node<Metadata>) => {
   const identifier = node.fragment.data;
-
   const newScope = new Scope(identifier);
-  const varValue = Expression.consumeNode(newScope, node.right!);
 
-  if (varValue !== undefined) {
-    const symbol = node.table.get(identifier)!;
-
-    if (symbol.data.literal) {
-
-      //parent.swap(parent.next);
-    }
-  }
+  Expression.consumeNode(newScope, node.right!);
 };
