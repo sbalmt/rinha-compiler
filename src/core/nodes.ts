007a58a42783fb9bfa75b46548fa7b30e4b5c2a9
Silas Balmante <silas@balmante.eti.br>
2023-09-26 13:37:17 -0300

    chore: better print for long fragments

--- src/core/nodes.ts
+++ src/core/nodes.ts
@@ -2,13 +2,19 @@ import * as Core from '@xcheme/core';
 
 import { serializeString } from './string';
 
+const MaxLength = 40;
+
+const formatString = (string: string) => {
+  return serializeString(string.length > MaxLength ? `${string.substring(0, MaxLength).trim()}...` : string);
+};
+
 export const printNodes = <T extends Core.Types>(node: Core.Node<T>, level: string = '', direction: string = 'T') => {
   const innerLevel = `${level}   `;
 
   while (node) {
     const fragment = node.fragment;
 
-    console.log(`${level} ${direction} ${node.value} ${serializeString(fragment.data)}`);
+    console.log(`${level} ${direction} ${node.value} ${formatString(fragment.data)}`);
 
     if (node.left) {
       printNodes(node.left, innerLevel, 'L');
