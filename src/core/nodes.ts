7927ae1cbb5c49dd3e194acf3b77d2954b5f30ba
Silas Balmante <silas@balmante.eti.br>
2023-09-26 16:57:01 -0300

    feat: print metadata details

--- src/core/nodes.ts
+++ src/core/nodes.ts
@@ -1,6 +1,8 @@
 import * as Core from '@xcheme/core';
 
 import { serializeString } from './string';
+import { Metadata } from './metadata';
+import { NodeTypes } from './types';
 
 const MaxLength = 40;
 
@@ -8,13 +10,24 @@ const formatString = (string: string) => {
   return serializeString(string.length > MaxLength ? `${string.substring(0, MaxLength).trim()}...` : string);
 };
 
-export const printNodes = <T extends Core.Types>(node: Core.Node<T>, level: string = '', direction: string = 'T') => {
+const getExtraDetails = (node: Core.Node<Metadata>) => {
+  if (node.value === NodeTypes.IDENTIFIER) {
+    const symbol = node.table.get(node.fragment);
+    const { references, mutable } = symbol!.data;
+
+    return `[RC: ${references ?? '?'}, IM: ${mutable ? 'F' : 'T'}]`;
+  }
+
+  return '';
+};
+
+export const printNodes = (node: Core.Node<Metadata>, level: string = '', direction: string = 'T') => {
   const innerLevel = `${level}   `;
 
   while (node) {
     const fragment = node.fragment;
 
-    console.log(`${level} ${direction} ${node.value} ${formatString(fragment.data)}`);
+    console.log(`${level} ${direction} ${node.value} ${getExtraDetails(node)} ${formatString(fragment.data)}`);
 
     if (node.left) {
       printNodes(node.left, innerLevel, 'L');
