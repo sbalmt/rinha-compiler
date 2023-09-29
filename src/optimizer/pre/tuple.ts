b20044df9606f6abc70dbddd8b24567b49e674c8
Silas Balmante <silas@balmante.eti.br>
2023-09-29 17:57:50 -0300

    refactor: make operation understandable

--- src/optimizer/pre/tuple.ts
+++ src/optimizer/pre/tuple.ts
@@ -6,12 +6,16 @@ import { Metadata, initNode } from '../../core/metadata';
 import { TupleTypes, ValueTypes } from '../../core/types';
 import { Scope } from '../scope';
 
+const detachTupleNodes = (node: Core.Node<Metadata>) => {
+  node.set(Core.NodeDirection.Right, undefined);
+};
+
 export function* consumeNode(scope: Scope, node: Core.Node<Metadata>): ValueTypes {
   const first = yield Expression.consumeNode(scope, node.right!);
   const second = yield Expression.consumeNode(scope, node.right!.next!);
   const value = [first, second] as TupleTypes;
 
-  node.set(Core.NodeDirection.Right, undefined);
+  detachTupleNodes(node);
 
   initNode(node, {
     value
