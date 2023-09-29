659fa949d06348f15d50ba6b4688bb17668fd3ab
Silas Balmante <silas@balmante.eti.br>
2023-09-28 23:25:04 -0300

    chore: adjust max stack and heap

--- docker/entry.sh
+++ docker/entry.sh
@@ -6,4 +6,4 @@ then
   file_location="/var/rinha/source.rinha"
 fi
 
-node --stack-size=10240 --max-old-space-size=1536 ./bin/rinha.js $file_location
+node --stack-size=51200 --max-old-space-size=1920 ./bin/rinha.js $file_location
--- tests/runner.sh
+++ tests/runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in ./tests/*.rinha; do
   echo " --- ${filename}"
-  node --stack-size=10240 --max-old-space-size=1536 ./bin/rinha.js $filename
+  node --stack-size=51200 --max-old-space-size=1920 ./bin/rinha.js $filename
 done
