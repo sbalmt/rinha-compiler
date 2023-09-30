2a143fbe3ad17c988fad23ea500934b28fc197e0
Silas Balmante <silas@balmante.eti.br>
2023-09-30 18:08:17 -0300

    refactor: better naming

--- src/optimizer/nodes/tuple.ts
+++ src/optimizer/nodes/tuple.ts
@@ -6,46 +6,46 @@ import { Metadata, initNode } from '../../core/metadata';
 import { TupleTypes, ValueTypes } from '../../core/types';
 import { Scope } from '../scope';
 
-const consumeInnerNode = (scope: Scope, value: ValueTypes) => {
+const consumeSingleValueNode = (scope: Scope, value: ValueTypes) => {
   if (value instanceof Core.Node) {
     return Expression.consumeNode(scope, value);
   }
 
   if (value instanceof Array) {
-    return consumeTupleNodes(scope, value);
+    return consumeTupleValueNodes(scope, value);
   }
 
   return value;
 };
 
-function* consumeTupleNodes(scope: Scope, tuple: TupleTypes): ValueTypes {
-  tuple[0] = yield consumeInnerNode(scope, tuple[0]);
-  tuple[1] = yield consumeInnerNode(scope, tuple[1]);
+function* consumeTupleValueNodes(scope: Scope, tuple: TupleTypes): ValueTypes {
+  tuple[0] = yield consumeSingleValueNode(scope, tuple[0]);
+  tuple[1] = yield consumeSingleValueNode(scope, tuple[1]);
 
   return tuple;
 }
 
-const detachTupleValueNode = (tupleNode: Core.Node<Metadata>) => {
+const detachTupleValueNodes = (tupleNode: Core.Node<Metadata>) => {
   tupleNode.set(Core.NodeDirection.Right, undefined);
 };
 
 export function* consumeNode(scope: Scope, tupleNode: Core.Node<Metadata>): ValueTypes {
   if (tupleNode.assigned) {
-    const tupleValue = tupleNode.data.value as TupleTypes;
-    return consumeTupleNodes(scope, tupleValue);
+    const tuple = tupleNode.data.value as TupleTypes;
+    return consumeTupleValueNodes(scope, tuple);
   }
 
-  const firstTupleNode = tupleNode.right!;
-  const secondTupleNode = firstTupleNode.next!;
+  const firstTupleValueNode = tupleNode.right!;
+  const secondTupleValueNode = firstTupleValueNode.next!;
 
   const data = initNode(tupleNode, {
     value: [
-      yield Expression.consumeNode(scope, firstTupleNode),
-      yield Expression.consumeNode(scope, secondTupleNode)
-    ] as TupleTypes
+      yield Expression.consumeNode(scope, firstTupleValueNode),
+      yield Expression.consumeNode(scope, secondTupleValueNode)
+    ]
   });
 
-  detachTupleValueNode(tupleNode);
+  detachTupleValueNodes(tupleNode);
 
   return data.value;
 }
